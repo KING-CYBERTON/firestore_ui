41467b1145700382e2291a7c3d36da3529980425
KING-CYBERTON <stevekim724@gmail.com>
2023-07-12 12:53:37 +0300

    edits

--- lib/Views/Form.dart
+++ lib/Views/Form.dart
@@ -41,92 +41,156 @@ class _ProfileTabState extends State<ProfileTab> {
       appBar: AppBar(
         title: Text('Profile'),
       ),
-      body: SingleChildScrollView(
-        child: Form(
-          key: _formKey,
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                const SizedBox(height: 20.0),
-                const Text(
-                  'Profile',
-                  style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold),
+      body: Center(
+        child: Container(
+       width: 350,
+              height: 600,
+              decoration: BoxDecoration(
+                image: const DecorationImage(
+                    image: AssetImage("assets/images/Splash.jpg"),
+                    fit: BoxFit.fill),
+                color: Colors.white,
+                boxShadow: const [
+                  BoxShadow(
+                    blurRadius: 10,
+                    offset: Offset(1, 1),
+                    color: Color.fromARGB(255, 145, 56, 115),
+                  )
+                ],
+                border: Border.all(
+                  color: Color.fromARGB(255, 139, 51, 103),
+                  width: 2,
                 ),
-                const SizedBox(height: 20.0),
-                TextFormField(
-                  controller: _UsernameController,
-                  decoration: const InputDecoration(
-                    labelText: 'UserName',
-                    border: OutlineInputBorder(),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your Username';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 20.0),
-                TextFormField(
-                  controller: _AgeController,
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(height: 20.0),
-                TextFormField(
-                  controller: _RegionController,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                    labelText: 'Period length (in days)',
-                    border: OutlineInputBorder(),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your period length';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 20.0),
-                TextFormField(
-                  controller: _NTPController,
-                  keyboardType: TextInputType.number,
-                  decoration: const InputDecoration(
-                    labelText: 'No of trees planted',
-                    border: OutlineInputBorder(),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your NO of trees';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 20.0),
-                TextFormField(
-                  controller: _BioController,
-                  decoration: const InputDecoration(
-                    labelText: 'Enter Bio data',
-                    border: OutlineInputBorder(),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a Bio data';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 20.0),
-                ElevatedButton(
-                  onPressed: () {
-                    Get.toNamed('Profile');
+                borderRadius: BorderRadius.circular(11),
+              ),
+          child: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: Center(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      const SizedBox(height: 20.0),
+                      const Text(
+                        'Profile',
+                        style: TextStyle(color: Colors.greenAccent, fontSize: 32.0, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 20.0),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.8),
+                          borderRadius: BorderRadius.circular(11),
+                        ),
+                        width: 300,
+                        
+                        child: TextFormField(
+                          
+                          controller: _UsernameController,
+                          decoration: const InputDecoration(
+                           fillColor: Colors.white,
+                            labelText: 'UserName',
+                            border: OutlineInputBorder(),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your Username';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      const SizedBox(height: 20.0),
+                      Container(
+                         decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.8),
+                          borderRadius: BorderRadius.circular(11),
+                        ),
+                        width: 300,
+                        child: TextFormField(
+                          controller: _AgeController,
+                          decoration: const InputDecoration(
+                            labelText: 'Email',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20.0),
+                      Container(
+                         decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.8),
+                          borderRadius: BorderRadius.circular(11),
+                        ),
+                        width: 300,
+                        child: TextFormField(
+                          controller: _RegionController,
+                          decoration: const InputDecoration(
+                            labelText: 'Enter your region',
+                            border: OutlineInputBorder(),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your region';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      const SizedBox(height: 20.0),
 
-                  },
-                  child: const Text('Save'),
-                ),
-              ]),
+                      Container(
+                         decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.8),
+                          borderRadius: BorderRadius.circular(11),
+                        ),
+                        width: 300,
+                        child: TextFormField(
+                          controller: _NTPController,
+                          keyboardType: TextInputType.number,
+                          decoration: const InputDecoration(
+                            labelText: 'No of trees planted',
+                            border: OutlineInputBorder(),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your NO of trees';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      const SizedBox(height: 20.0),
+                      Container(
+                         decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.8),
+                          borderRadius: BorderRadius.circular(11),
+                        ),
+                        width: 300,
+                        child: TextFormField(
+                          controller: _BioController,
+                          decoration: const InputDecoration(
+                            labelText: 'Enter Bio data',
+                            border: OutlineInputBorder(),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter a Bio data';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      const SizedBox(height: 20.0),
+                      ElevatedButton(
+                        onPressed: () {
+                          Get.toNamed('Profile');
+                      
+                        },
+                        child: const Text('Save'),
+                      ),
+                    ]),
+              ),
+            ),
+          ),
         ),
       ),
     );
--- lib/Views/Profile.dart
+++ lib/Views/Profile.dart
@@ -24,174 +24,195 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
           },
         ),
       ]),
-      body:Container(
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.pink.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: const Offset(0, 3), // changes position of shadow
+      body:Center(
+        child: Container(
+                 width: 350,
+                 height: 600,
+                
+                decoration: BoxDecoration(
+                  image: const DecorationImage(
+                      image: AssetImage("assets/images/Splash.jpg"),
+                      fit: BoxFit.fill),
+                  color: Colors.white,
+                  boxShadow: const [
+                    BoxShadow(
+                      blurRadius: 10,
+                      offset: Offset(1, 1),
+                      color: Color.fromARGB(255, 145, 56, 115),
+                    )
+                  ],
+                  border: Border.all(
+                    color: Color.fromARGB(255, 139, 51, 103),
+                    width: 2,
                   ),
-                ],
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white,
+                  borderRadius: BorderRadius.circular(11),
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
+                      
+                    children: [
+                      const SizedBox(height: 30),
+                      const CircleAvatar(
+                        radius: 100,
+                        backgroundColor: Colors.transparent,
+                        backgroundImage: AssetImage('assets/images/logo.png'),
+                      ),
+                      
+                      Container(
+                        width: 300,
+                        decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 46, 75, 61).withOpacity(0.5),
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 2,
+                              blurRadius: 5,
+                              offset: const Offset(0, 3),
+                            ),
+                          ],
+                        ),
+                        padding: const EdgeInsets.all(10),
+                        child: const Text(
+                          " Name: John Doe ",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                        Container(
+                              width: 300,
+                              decoration: BoxDecoration(
+                                 color: Color.fromARGB(255, 46, 75, 61).withOpacity(0.5),
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.5),
+                                    spreadRadius: 2,
+                                    blurRadius: 5,
+                                    offset: const Offset(0, 3),
+                                  ),
+                                ],
+                              ),
+                              padding: const EdgeInsets.all(10),
+                              child: const Text(
+                                "Biograpy: I am free spirited and energetic on the green belt movement",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 20),
+                      
+                      Container(
+                        width: 300,
+                        decoration: BoxDecoration(
+                           color: Color.fromARGB(255, 46, 75, 61).withOpacity(0.5),
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 2,
+                              blurRadius: 5,
+                              offset: const Offset(0, 3),
+                            ),
+                          ],
+                        ),
+                        padding: const EdgeInsets.all(10),
+                        child: const Text(
+                          "Region: Asia  ",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Center(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: 300,
+                              decoration: BoxDecoration(
+                                 color: Color.fromARGB(255, 46, 75, 61).withOpacity(0.5),
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.5),
+                                    spreadRadius: 2,
+                                    blurRadius: 5,
+                                    offset: const Offset(0, 3),
+                                  ),
+                                ],
+                              ),
+                              padding: const EdgeInsets.all(10),
+                              child: const Text(
+                                "Age: 24",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 20),
+                            Container(
+                              width: 300,
+                              decoration: BoxDecoration(
+                                 color: Color.fromARGB(255, 46, 75, 61).withOpacity(0.5),
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.5),
+                                    spreadRadius: 2,
+                                    blurRadius: 5,
+                                    offset: const Offset(0, 3),
+                                  ),
+                                ],
+                              ),
+                              padding: const EdgeInsets.all(10),
+                              child: const Text(
+                                "No of trees: 29 ",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 20),
+                          
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          // GetAuth.instance.logOut();
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.red,
+                          shadowColor: const Color.fromARGB(26, 81, 160, 180),
+                        ),
+                        child: Text(
+                          'Log out',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.lightGreen[800],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              child: Column(
-                children: [
-                  const SizedBox(height: 30),
-                  const CircleAvatar(
-                    radius: 120,
-                    //backgroundColor: Colors.white70,
-                    backgroundImage: AssetImage('assets/images/logo.png'),
-                  ),
-                  const SizedBox(height: 20),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.pinkAccent,
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 2,
-                          blurRadius: 5,
-                          offset: const Offset(0, 3),
-                        ),
-                      ],
-                    ),
-                    padding: const EdgeInsets.all(10),
-                    child: const Text(
-                      " {userData.username}",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        fontStyle: FontStyle.italic,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.pinkAccent,
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 2,
-                          blurRadius: 5,
-                          offset: const Offset(0, 3),
-                        ),
-                      ],
-                    ),
-                    padding: const EdgeInsets.all(10),
-                    child: const Text(
-                      "Region:  {userData.region}",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                        fontStyle: FontStyle.italic,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  Center(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Colors.pink,
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.5),
-                                spreadRadius: 2,
-                                blurRadius: 5,
-                                offset: const Offset(0, 3),
-                              ),
-                            ],
-                          ),
-                          padding: const EdgeInsets.all(10),
-                          child: const Text(
-                            "Age: {userData.age}",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Colors.pink,
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.5),
-                                spreadRadius: 2,
-                                blurRadius: 5,
-                                offset: const Offset(0, 3),
-                              ),
-                            ],
-                          ),
-                          padding: const EdgeInsets.all(10),
-                          child: const Text(
-                            "no_of_trees: {userData.no_of_trees}",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Colors.pink,
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.5),
-                                spreadRadius: 2,
-                                blurRadius: 5,
-                                offset: const Offset(0, 3),
-                              ),
-                            ],
-                          ),
-                          padding: const EdgeInsets.all(10),
-                          child: const Text(
-                            "Biograpy: {userData.biography}",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 90),
-                  ElevatedButton(
-                    onPressed: () {
-                      // GetAuth.instance.logOut();
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.red,
-                      shadowColor: const Color.fromARGB(26, 81, 160, 180),
-                    ),
-                    child: Text(
-                      'Log out',
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.lightGreen[800],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            )
-            );
-        
+      )
+    );        
 }
 }
